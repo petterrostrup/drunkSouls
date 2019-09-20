ff2bc8474fe05ebf4c09faf3a2669fa35b9c0d22
Aleksander Dahl <aleksanderdahl@hotmail.com>
2019-09-20 17:09:11 +0200

    ISSUE-REWRITE-2: Added covenants, styled them, and routing to covenants

--- drunk-souls/src/covenants/covenants.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-class Covenants extends React.Component {
-  render() {
-    return <h1>Covenants</h1>
-  }
-}
-export default Covenants
---
+++ drunk-souls/src/covenants/warrior-of-sunlight/WarriorOfSunlight.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class WarriorOfSunlight extends React.Component {
+  render() {
+    return (
+      <div>
+          <p>Warrior Of Sunlight</p>
+      </div>
+    )
+  }
+}
+export default WarriorOfSunlight
