3293ceaf78901cbbe398996a98a602d8b1afe02a
root <root@CUPCAKE-LAPTOP.localdomain>
2019-11-04 18:22:11 +0100

    Restructure: Changed name to capital and moved text to own file

---
+++ drunk-souls/src/rules/Rules.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RulesText from './RulesText';
+
+class Rules extends React.Component {
+  render() {
+    document.title = "Drunk Souls - Rules";
+    return (
+      <RulesText/>
+    )
+  }
+}
+export default Rules
